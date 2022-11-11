0e32a44b7741a2ff7ba209caf0c0125ecfe35696
Mathieu Vignau <mvignau@snapchat.com>
2022-11-11 15:04:06 -0800

    Add Swift Package support

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "OpenMultitouchSupport",
+    products: [
+        .library(
+            name: "OpenMultitouchSupport",
+            targets: ["OpenMultitouchSupport"]),
+    ],
+    targets: [
+        .target(
+            name: "OpenMultitouchSupport",
+            path: "OpenMultitouchSupport",
+            publicHeadersPath: ".",
+            linkerSettings: [
+                .linkedFramework("MultitouchSupport"),
+                .unsafeFlags(["-F", "/System/Library/PrivateFrameworks"]),
+                .linkedFramework("Cocoa"),
+            ]
+        ),
+    ]
+)
